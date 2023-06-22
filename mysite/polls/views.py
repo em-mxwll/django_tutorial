6c3dea184ae984a274e815e3a69a4c0dc09cd96d
tiredslytherclaw <m00nlitsouls03@gmail.com>
2023-06-22 16:00:48 -0400

    completed part 4 of tutorial

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,37 +1,29 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
-from django.urls import path
-from . import views
-from .models import Question
-from django.template import loader
-from django.http import Http404
+from django.urls import reverse
+from django.views import generic
 
-# Create your views here.
-def index(request):
-    #return HttpResponse("Hello, world. You're at the polls index.")
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+from .models import Choice, Question
 
-def detail(request, question_id):
-    #return HttpResponse("You're looking at question %s." % question_id)
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-
-urlpatterns = [
-    # ex: /polls/
-    path("", views.index, name="index"),
-    # ex: /polls/5/
-    path("<int:question_id>/", views.detail, name="detail"),
-    # ex: /polls/5/results/
-    path("<int:question_id>/results/", views.results, name="results"),
-    # ex: /polls/5/vote/
-    path("<int:question_id>/vote/", views.vote, name="vote"),
-]
+    ...  # same as above, no changes needed.
